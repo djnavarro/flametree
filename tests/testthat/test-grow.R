e2d67968a5d5306210c6939269e957a25489c07d
Danielle Navarro <d.navarro@unsw.edu.au>
2021-04-20 22:43:50 +1000

    adds tests for seed parameter

---
+++ tests/testthat/test-grow.R
@@ -0,0 +1,29 @@
+with_attributes <- function(x, value = list(blah = "blah")) {
+  attributes(x) <- value
+  return(x)
+}
+
+test_that("invalid seeds are forbidden", {
+
+  # individual error checks (check that the correct error is thrown)
+  expect_error(flametree_grow(seed = NULL), "must not be null")
+  expect_error(flametree_grow(seed = NA), "must not contain missing values")
+  expect_error(flametree_grow(seed = .1234), "must be integer valued")
+  expect_error(flametree_grow(seed = 1:3), "must have length")
+  expect_error(flametree_grow(seed = numeric(0L)), "must have length")
+  expect_error(flametree_grow(seed = "abc"), "must be numeric")
+  expect_error(flametree_grow(seed = TRUE), "must be numeric")
+  expect_error(flametree_grow(seed = list(1)), "must be numeric")
+
+  # combination error checks (don't care which error is thrown, just
+  # checking that weird edge cases don't escape the checks)
+  expect_error(flametree_grow(seed = c(1, NA)))
+  expect_error(flametree_grow(seed = list(1, NA)))
+
+  # edge cases that should pass without messages or warnings
+  expect_silent(flametree_grow(seed = 0)) # zero ok
+  expect_silent(flametree_grow(seed = -1)) # negative numbers okay
+  expect_silent(flametree_grow(seed = c("name" = 3))) # named integers ok
+  expect_silent(flametree_grow(seed = with_attributes(3))) # integers with attributes ok
+
+})
